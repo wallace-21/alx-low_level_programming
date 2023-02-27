89eaec7fce3eca7ce5ce408b6c5b21679c245d3c
wallace-21 <exnesswallace@gmail.com>
2023-02-27 11:34:50 -0800

    my first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * @n: an integer input pointer
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
