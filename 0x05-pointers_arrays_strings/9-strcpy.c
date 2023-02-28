aa78026d856f7ff08719a15959bef9e934454996
wallace-21 <exnesswallace@gmail.com>
2023-02-28 14:38:24 -0800

    my tenth commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: Destination value
+ * @src: Source value
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int a;
+for (a = 0; src[a] != '\0'; a++)
+{
+dest[a] = src[a];
+}
+dest[a++] = '\0';
+return (dest);
+}
