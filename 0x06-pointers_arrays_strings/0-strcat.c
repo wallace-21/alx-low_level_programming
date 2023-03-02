d2388d55311fe23750ea90d3180ce5b69e3b4265
wallace-21 <exnesswallace@gmail.com>
2023-03-02 06:28:46 -0800

    my first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int man = 0, i;
+
+	while (dest[man])
+	{
+		man++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[man] = src[i];
+		man++;
+	}
+
+	dest[man] = '\0';
+	return (dest);
+}
