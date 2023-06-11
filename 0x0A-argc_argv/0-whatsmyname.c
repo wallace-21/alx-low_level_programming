ac699335edc48907980ed351ef81f5a0fff11c90
wallace <exnesswallace@gmail.com>
2023-06-11 07:09:32 -0700

    command-line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - starting point of the program execution
+ * @argc: contains the number of arguments passed to the program
+ * @argv: a one-dimensional array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
