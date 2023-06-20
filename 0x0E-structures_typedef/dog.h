2835339c6a84b33eb284c5f7d1dfb50efad8c31e
wallace <exnesswallace@gmail.com>
2023-06-19 17:48:16 -0700

    my struct and README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - Define a new data type
+ * @name: string
+ * @age: float
+ * @owner: string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
