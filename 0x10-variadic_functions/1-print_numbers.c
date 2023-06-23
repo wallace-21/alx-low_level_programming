fb37ef31323c4bbf8e40cc6c46f6cf7d7a0508f5
wallace <exnesswallace@gmail.com>
2023-06-23 06:13:31 -0700

    print numbers using macros

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ * Return: pnum 
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int pnum = va_arg(args,int);
+		printf("%d", pnum);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end (args);
+}
