f99f997a583eff747fa567c10cbac45f550a2a69
wallace <exnesswallace@gmail.com>
2023-06-23 04:32:21 -0700

    sum of functions parameters using macros

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: index
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	int sum = 0;
+	unsigned int i;
+	
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0)
+		{
+			return (0);
+		}
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
+
+	return(sum);
+}
