d79d4e9e43935f4278798f66bc2e1185ce2364fc
wallace <exnesswallace@gmail.com>
2023-06-29 17:08:31 -0700

    forgot about betty and comments

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,10 +3,10 @@
 
 /**
  * list_len - function that returns the number of elements in a
- *linked list_t list.
- @h: pointer variable
-Return
-*/
+ * linked list_t list.
+ * @h: pointer variable
+ * Return: nodes
+ */
 
 size_t list_len(const list_t *h)
 {
