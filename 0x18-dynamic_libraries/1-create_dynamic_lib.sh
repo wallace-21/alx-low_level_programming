5b62a9c9548def2d8c8cbcddb8d19d20eb689bbe
wallace-21 <exnesswallace@gmail.com>
2023-08-21 13:48:18 -0700

    changed and simplified it

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c isupper.c strcat.c isdigit.c strncat.c strlen.c islower.c strcmp.c isalpha.c strstr.c strcpy.c memset.c atoi.c memcpy.c strchr.c strncpy.c puts.c strspn.c strpbrk.c abs.c putchar.c -fpic
-gcc *.o -shared -o liball.so
+gcc -c *.c -fpic
+gcc -shared -o liball.so *.o
