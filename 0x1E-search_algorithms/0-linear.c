1953dd30e0357539fb7dc448f5807f5ccc5ee127
wallace <exnesswallace@gmail.com>
2024-02-07 00:10:54 +0200

    added the values being checked one by one

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,8 +18,10 @@ int linear_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
+
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked at array [%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
