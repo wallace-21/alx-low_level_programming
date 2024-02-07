ff915a7b8dfce6c0576a9ac8d2ffca832d7db125
wallace <exnesswallace@gmail.com>
2024-02-08 01:23:53 +0200

    added the array output

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 /**
+ * print_array - function that prints the contents of an array
+ * @a: pointer to the first element of the array
+ * @n: number of elements in the array
+ */
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < (n - 1); b++)
+	{
+		printf("%d, ", a[b]);
+	}
+
+	if (b == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
+
+/**
  * binary_search - function that searches for a value in a sorted array of
  * integers using the Linear search algorithm
  * @array: pointer to the first element of the array to search in
@@ -15,8 +37,12 @@ int binary_search(int *array, size_t size, int value)
 	size_t end = size - 1;
 	size_t mid;
 
+	/*printf("Searching in array: ");*/
 	while (start <= end)
 	{
+		printf("Searching in array: ");
+		print_array(array, size);
+
 		mid = start + (end - start) / 2;
 
 		if (value == array[mid])
